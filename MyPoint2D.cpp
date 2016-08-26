4e48501eed9d06f98fc451908cdb67a55b5fb3fc
Szabolcs Dombi <cprogrammer1994@gmail.com>
2016-08-26 16:01:36 +0300

    point example

---
+++ MyPoint2D.cpp
@@ -0,0 +1,31 @@
+class MyPoint2D {
+public:
+	MyVec2D position;
+	MyVec2D previous;
+	MyVec2D acceleration;
+
+	MyPoint2D();
+	MyPoint2D(const MyVec2D & position);
+
+	void accelerate(const MyVec2D & vec);
+	void correct(const MyVec2D & vec);
+	void simulate(float delta);
+};
+
+MyPoint2D::MyPoint2D() {
+}
+
+MyPoint2D::MyPoint2D(const MyVec2D & position) : position(position), previous(position) {
+}
+
+void MyPoint2D::accelerate(const MyVec2D & vec) {
+	acceleration += vec;
+}
+
+void MyPoint2D::correct(const MyVec2D & vec) {
+	position += vec;
+}
+
+void MyPoint2D::simulate(float delta) {
+	//
+}
