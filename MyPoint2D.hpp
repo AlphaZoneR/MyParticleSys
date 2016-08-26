bda681d5772529224e04b06c0c0d60b0e56b9597
Szabolcs Dombi <cprogrammer1994@gmail.com>
2016-08-26 16:07:46 +0300

    header

---
+++ MyPoint2D.hpp
@@ -0,0 +1,13 @@
+class MyPoint2D {
+public:
+	MyVec2D position;
+	MyVec2D previous;
+	MyVec2D acceleration;
+
+	MyPoint2D();
+	MyPoint2D(const MyVec2D & position);
+
+	void accelerate(const MyVec2D & vec);
+	void correct(const MyVec2D & vec);
+	void simulate(float delta);
+};
