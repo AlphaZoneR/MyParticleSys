1749a7ce83e1323824ae90913308ef86fa967389
AlphaZoneR <frike98@yahoo.com>
2016-08-26 16:03:29 +0300

    headers

---
+++ MyVec2.hpp
@@ -0,0 +1,8 @@
+class MyVec2D {
+public:
+	float x, y;
+
+	MyVec2D();
+	MyVec2D(const MyVec2D & copy);
+	void normalize();
+};
--- MyVec2D.cpp
+++ MyVec2D.cpp
@@ -1,10 +1,5 @@
-class MyVec2D {
-public:
-	float x, y;
+#include "MyVec2D.hpp"
 
-	MyVec2D();
-	MyVec2D(const MyVec2D & copy);
-};
 
 MyVec2D operator +(const MyVec2D & a, const MyVec2D & b);
 
@@ -17,15 +12,43 @@ MyVec2D::MyVec2D(const MyVec2D & copy) : x(copy.x), y(copy.y) {
 MyVec2D operator +(const MyVec2D & a, const MyVec2D & b) {
 	return MyVec2D(a.x + b.x, a.y + b.y);
 }
-
-MyVec2D operator *(const MyVec2D & a, float b) {
-	return MyVec2D(a.x * f, a.y * f);
-}
-
-MyVec2D operator *(float a, const MyVec2D & b) {
-	return MyVec2D(f * b.x, f * b.y);
-}
-
 void operator +=(MyVec2D & a, const MyVec2D & b) {
 	a = a + b;
 }
+
+void operator -=(MyVec2D & a, const MyVec2D & b) {
+	a = a - b;
+}
+
+MyVec2D operator -(const MyVec2D & a, const MyVec2D & b) {
+	return MyVec2D(a.x - b.x, a.y - b.y);
+}
+
+
+MyVec2D operator *(const MyVec2D & a, float b) {
+	return MyVec2D(a.x * b, a.y * b);
+}
+
+MyVec2D operator *(float a, const MyVec2D & b) {
+	return MyVec2D(a * b.x, a * b.y);
+}
+
+MyVec2D operator /(const MyVec2D & a, float b) {
+	return MyVec2D(a.x / b, a.y / b);
+}
+
+MyVec2D operator /(float a, const MyVec2D & b) {
+	return MyVec2D(b / b.x, b * b.y);
+}
+
+void MyVec2D::normalize(){
+
+
+}
+
+
+
+
+
+
+
